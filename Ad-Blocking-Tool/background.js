8d946667bbac78d9401d606aa0a89dc7f6f5df2c
Davi koth <iurikoth@gmail.com>
2021-07-11 11:34:53 -0300

    Create ad blocker

---
+++ Ad-Blocking-Tool/background.js
@@ -0,0 +1,19 @@
+const defaultFilters = [
+        "*://adinplay.com/*",
+	"*://*.doubleclick.net/*",
+	"*://partner.googleadservices.com/*",
+	"*://*.googlesyndication.com/*",
+	"*://*.google-analytics.com/*",
+	"*://creative.ak.fbcdn.net/*",
+	"*://*.adbrite.com/*",
+	"*://*.exponential.com/*",
+	"*://*.quantserve.com/*",
+	"*://*.scorecardresearch.com/*",
+	"*://*.zedo.com/*",
+]
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) { return { cancel: true }},
+    { urls: defaultFilters },
+    ["blocking"]
+)
